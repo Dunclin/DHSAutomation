e59d4adb528020a3d00e8d38c5a3667dba9f7713
Dunclin <dunclin@hotmail.com>
2025-08-15 01:40:15 -0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// ===== Zoom plugin configuration =====
-window.ZOOM_SDK_ENABLED = false; // set to true to allow in-browser join
-window.ZOOM_SIGNATURE_ENDPOINT = ""; // e.g., https://your-cloud-function/zoomSignature
-window.ZOOM_SDK_KEY = "";
